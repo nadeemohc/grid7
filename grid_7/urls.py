aada7ac21b46c7bdac859f68da3bc8aa585d0bfc
Muhammed Nadeem <nadeemvezhappilly@gmail.com>
2024-03-10 11:45:47 +0530

    going to flush the migration and connect a new databse inorder to add the django.contrib.sites to installed apps

--- grid_7/urls.py
+++ grid_7/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', include('store.urls')),
     path('accounts/', include('allauth.urls')),
     path('accounts/', include('allauth.socialaccount.urls')),
+    # path('accounts/google/login/' TemplateView.as_view(template_name='')))
     path('home', TemplateView.as_view(template_name='dashboard/home.html'), name='home')
 ]
