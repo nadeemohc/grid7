8154f3afeab46e713c12b7411db93d7bf5098201
Muhammed Nadeem <nadeemvezhappilly@gmail.com>
2024-03-13 21:59:53 +0530

    Last update: Added Category model Need to add Product model, Make Product display dynamic resend button etc.

--- cust_admin/views.py
+++ cust_admin/views.py
@@ -1,16 +1,35 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import get_user_model
 from accounts.models import User
+from django.contrib import messages
 
 def dashboard(request):
     return render(request, 'cust_admin/index.html', {'title':'Admin Dashboard'})
 
 def user_list(request):
-    users = User.objects.all()
-    return render(request, 'cust_admin/user_list.html', {'title':'User List', 'users': users})
+    users = User.objects.all().order_by('id')
+    return render(request, 'cust_admin/user/user_list.html', {'title':'User List', 'users': users})
 
-def admin_view_user(request, username):
+def user_view(request, username):
     user = get_object_or_404(User, username=username)
-    return render(request, 'cust_admin/view_user.html', {'title': 'View User', 'user': user})
+    # context = {
+    #     'user
+    # }
+    return render(request, 'cust_admin/user/user_view.html', {'title': 'View User', 'user': user})
 
 User = get_user_model()
+
+
+def user_block_unblock(request, username):
+    user = get_object_or_404(User, username = username)
+    user.is_active = not user.is_active
+    user.save()
+    action = 'blocked' if not user.is_active else 'unblocked'
+    messages.success(request, f"The user {user.username} has been {action} successfully.")
+    return redirect('cust_admin:user_list')
+
+def add_product(request):
+    return render(request, 'cust_admin/product/product_add.html', {'title':'Add Product'})
+
+def prod_list(request):
+    return render(request, 'cust_admin/product/product_list.html', {'title':'Product List'})
--- store/models.py
+++ store/models.py
@@ -1,3 +1,28 @@
 from django.db import models
-
+from django.utils.safestring import mark_safe
 # Create your models here.
+
+class Category(models.Model):
+    cid = models.BigAutoField(unique=True, primary_key=True)
+    cname = models.CharField(max_length = 50)
+    image = models.ImageField(upload_to='category',default='category.jpg')
+    is_blocked = models.BooleanField(default=False)
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def category_image(self):
+        if self.image:
+            return mark_safe('<img src="%s" width="50" height="50" />' % (self.image.url))
+        else:
+            return "NO Image Available"
+
+    def __str__(self):
+        return self.cname
+    
+class Subcategory(models.Model):
+    sid = models.BigAutoField(unique=True, primary_key = True)
+    sub_name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, related_name="subcategories",db_column = 'cid')
+
+    def __str__(self):
+        return self.sub_name
